501ae3004ab0da187425b580d6cf21b09470b0b0
Marcin Dubel <marcin@appsilon.com>
2022-05-02 10:40:52 +0200

    Update dependencies with shinyloadtest and shinytest2

--- dependencies.R
+++ dependencies.R
@@ -1,4 +1,5 @@
 # This file allows packrat (used by rsconnect during deployment) to pick up dependencies.
 library(rhino)
-library(shinytest)
+library(shinytest2)
+library(shinyloadtest)
 library(data.validator)
