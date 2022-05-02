0ac8922b4149b121fbbfa1bf481d3cdd0dc57ea0
Marcin Dubel <marcin@appsilon.com>
2022-05-02 11:26:46 +0200

    Add unit tests

---
+++ tests/testthat/test-data.R
@@ -0,0 +1,35 @@
+box::use(
+  shiny[testServer],
+  testthat[...],
+)
+box::use(
+  app/logic/data[...],
+)
+
+test_that("get_column_multiplied returns column by name in matrix", {
+  matrix <- datasets::WorldPhones
+  selected_column <- sample(colnames(matrix), 1)
+  expect_equal(
+    matrix[,selected_column],
+    get_column_multiplied(matrix, selected_column, 1)
+  )
+})
+
+test_that("get_column_multiplied returns column multiplied by given value", {
+  matrix <- datasets::WorldPhones
+  selected_column <- sample(colnames(matrix), 1)
+  random_value <- sample(1:100, 1)
+  expect_equal(
+    matrix[,selected_column] * random_value,
+    get_column_multiplied(matrix, selected_column, random_value)
+  )
+})
+
+test_that("get_column_multiplied returns column multiplied by 1000 by default", {
+  matrix <- datasets::WorldPhones
+  selected_column <- sample(colnames(matrix), 1)
+  expect_equal(
+    matrix[,selected_column] * 1000,
+    get_column_multiplied(matrix, selected_column)
+  )
+})
