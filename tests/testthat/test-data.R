6f42a7e7b47944b4900e4f653f4e6b13374b46a3
Marcin Dubel <marcin@appsilon.com>
2022-10-17 18:43:10 +0200

    Fix loading packages for testing data

--- tests/testthat/test-data.R
+++ tests/testthat/test-data.R
@@ -1,13 +1,10 @@
-box::use(
-  data.validator[...],
-  assertr[...],
-  testthat[...],
-)
+library(assertr)
+library(data.validator)
 
 test_that("WorldPhones follows assumptions in the app", {
   dataset <- data.frame(datasets::WorldPhones)
   report <- data_validation_report()
-  
+
   validate(dataset, name = "WorldPhones dataset") %>% 
     validate_if(has_all_names("N.Amer", "Europe", "Asia", "S.Amer", "Oceania", "Africa", "Mid.Amer")) %>% 
     validate_cols(within_bounds(0, Inf), everything(), description = "All values are positive") %>% 
